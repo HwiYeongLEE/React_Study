12182eb7dcfc88d95040d0b24d80678a06a5ce9b
unknown <2gnldud@gmail.com>
2022-09-18 11:03:26 +0900

    2022/09/18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 className="App">블로그 만들기</h3>
+      <Posting title='new title1'/>
+      <Posting title='new title2'/>
+      <Posting title='new title3'/>
     </div>
   );
+}
+function Posting(prop) {
+  let [like, setLike] = useState(0)
+  return(
+    <div className="App">
+      {prop.title}
+      <span onClick={()=> setLike(like+1)}>  LIKE 👍</span>
+      {like}
+    </div>
+  )
 }
 
 export default App;
